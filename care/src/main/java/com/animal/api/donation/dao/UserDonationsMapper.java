8c6bbb7e98cf13df6b57b7c76d84148ba6462fc5
consgary <sesgodgood3@naver.com>
2025-06-18 20:22:36 +0900

    ✨ Feat: add dao

--- care/src/main/java/com/animal/api/donation/dao/UserDonationsMapper.java
+++ care/src/main/java/com/animal/api/donation/dao/UserDonationsMapper.java
@@ -1,8 +1,14 @@
 package com.animal.api.donation.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.animal.api.donation.model.response.AllDonationListResponseDTO;
 
 @Mapper
 public interface UserDonationsMapper {
 
+	public List<AllDonationListResponseDTO> getAllDonations(Map map);
 }
