7b9d8bcc1d4c9bb38e1ad869341d3d44beb4d150
consgary <sesgodgood3@naver.com>
2025-06-18 20:30:15 +0900

    ✨ Feat:add service

--- care/src/main/java/com/animal/api/donation/service/UserDonationsService.java
+++ care/src/main/java/com/animal/api/donation/service/UserDonationsService.java
@@ -1,5 +1,11 @@
 package com.animal.api.donation.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.animal.api.donation.model.response.AllDonationListResponseDTO;
+
 public interface UserDonationsService {
 
+	public List<AllDonationListResponseDTO> getAllDonations(int listSize, int cp);
 }
--- care/src/main/java/com/animal/api/donation/service/UserDonationsServiceImple.java
+++ care/src/main/java/com/animal/api/donation/service/UserDonationsServiceImple.java
@@ -1,8 +1,29 @@
 package com.animal.api.donation.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.animal.api.donation.dao.UserDonationsMapper;
+import com.animal.api.donation.model.response.AllDonationListResponseDTO;
 
 @Service
 public class UserDonationsServiceImple implements UserDonationsService {
-
+	
+	@Autowired
+	private UserDonationsMapper mapper;
+	
+	
+	@Override
+	public List<AllDonationListResponseDTO> getAllDonations(int listSize, int cp) {
+		Map<String,Integer> map=new HashMap<String,Integer>();
+		map.put("listSize", listSize);
+		map.put("cp", cp);
+		List<AllDonationListResponseDTO> donationList=mapper.getAllDonations(map);
+		
+		return donationList;
+	}
 }
