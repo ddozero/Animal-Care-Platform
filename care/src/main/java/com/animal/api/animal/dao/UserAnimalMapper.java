25c8a18620c83d36021a4699e9ec2b28944b9f13
Rege-97 <cogus1027@gmail.com>
2025-06-18 16:23:50 +0900

    ✨ Feat: add mapper and service

--- care/src/main/java/com/animal/api/animal/dao/UserAnimalMapper.java
+++ care/src/main/java/com/animal/api/animal/dao/UserAnimalMapper.java
@@ -1,8 +1,13 @@
 package com.animal.api.animal.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.animal.api.animal.model.response.AllAnimalListResponseDTO;
 
 @Mapper
 public interface UserAnimalMapper {
-
+	public List<AllAnimalListResponseDTO> getAllAnimals(Map map);
 }
