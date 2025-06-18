12f199daddce774997d90afa4b38a88ded7dc791
consgary <sesgodgood3@naver.com>
2025-06-18 21:19:00 +0900

    ✨ Feat: add controller and update service

--- care/src/main/java/com/animal/api/donation/controller/UserDonationsController.java
+++ care/src/main/java/com/animal/api/donation/controller/UserDonationsController.java
@@ -1,8 +1,40 @@
 package com.animal.api.donation.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.animal.api.common.model.ErrorResponseDTO;
+import com.animal.api.common.model.OkResponseDTO;
+import com.animal.api.donation.model.response.AllDonationListResponseDTO;
+import com.animal.api.donation.service.UserDonationsService;
+
 @RestController
+@RequestMapping("/api/donations")
 public class UserDonationsController {
+
+	@Autowired
+	private UserDonationsService service;
+
+	@GetMapping
+	public ResponseEntity<?> getAllDonationsLists(@RequestParam(value = "cp", defaultValue = "0") int cp) {
+		int listSize = 3;
+		List<AllDonationListResponseDTO> donationList = service.getAllDonations(listSize, cp);
+
+		if (donationList == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(400, "잘못된 요청"));
+		} else if (donationList.size() == 0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(404, "데이터가 존재하지않음"));
+		} else {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new OkResponseDTO<List<AllDonationListResponseDTO>>(200, "기부 전체 조회 성공", donationList));
+		}
+	}
 
 }
--- care/src/main/java/com/animal/api/donation/service/UserDonationsServiceImple.java
+++ care/src/main/java/com/animal/api/donation/service/UserDonationsServiceImple.java
@@ -5,25 +5,32 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.animal.api.donation.dao.UserDonationsMapper;
 import com.animal.api.donation.model.response.AllDonationListResponseDTO;
 
 @Service
+@Primary
 public class UserDonationsServiceImple implements UserDonationsService {
-	
+
 	@Autowired
 	private UserDonationsMapper mapper;
-	
-	
+
 	@Override
 	public List<AllDonationListResponseDTO> getAllDonations(int listSize, int cp) {
-		Map<String,Integer> map=new HashMap<String,Integer>();
+
+		if (cp == 0) {
+			cp = 1;
+		}
+		cp = (cp - 1) * listSize;
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
 		map.put("listSize", listSize);
 		map.put("cp", cp);
-		List<AllDonationListResponseDTO> donationList=mapper.getAllDonations(map);
-		
+		List<AllDonationListResponseDTO> donationList = mapper.getAllDonations(map);
+
 		return donationList;
 	}
 }
