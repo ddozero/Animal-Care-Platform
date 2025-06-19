24c3c52e8a00996d6e3a6873dc1d8a7b5b8ea7d5
Rege-97 <cogus1027@gmail.com>
2025-06-19 14:20:32 +0900

    ✨ Feat: add service

--- care/src/main/java/com/animal/api/shelter/service/UserShelterService.java
+++ care/src/main/java/com/animal/api/shelter/service/UserShelterService.java
@@ -1,5 +1,10 @@
 package com.animal.api.shelter.service;
 
+import java.util.List;
+
+import com.animal.api.shelter.model.response.AllShelterListDTO;
+
 public interface UserShelterService {
 
+	public List<AllShelterListDTO> getAllShelters(int listSize, int cp);
 }
