b79b610d16398d32f8abd4670d2c6ca72500c8b1
Rege-97 <cogus1027@gmail.com>
2025-06-19 14:29:09 +0900

    ✨ Feat: add controller and test ok

--- care/src/main/java/com/animal/api/shelter/controller/UserShelterController.java
+++ care/src/main/java/com/animal/api/shelter/controller/UserShelterController.java
@@ -1,9 +1,18 @@
 package com.animal.api.shelter.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.animal.api.common.model.ErrorResponseDTO;
+import com.animal.api.common.model.OkResponseDTO;
+import com.animal.api.shelter.model.response.AllShelterListDTO;
 import com.animal.api.shelter.service.UserShelterService;
 
 @RestController
@@ -12,4 +21,21 @@ public class UserShelterController {
 
 	@Autowired
 	private UserShelterService service;
+
+	@GetMapping
+	public ResponseEntity<?> getShelters(@RequestParam(value = "cp", defaultValue = "0") int cp) {
+		int listSize = 3;
+
+		List<AllShelterListDTO> shelterList = service.getAllShelters(listSize, cp);
+
+		if (shelterList == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(400, "잘못된 접근입니다."));
+		} else if (shelterList.size() == 0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(404, "조회된 데이터가 없습니다."));
+		} else {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new OkResponseDTO<List<AllShelterListDTO>>(200, "조회 성공", shelterList));
+		}
+	}
+
 }
