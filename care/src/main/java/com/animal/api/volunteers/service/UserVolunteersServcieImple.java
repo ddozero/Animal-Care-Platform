eade210ebe965834e04c9e7fe971ed5a1d9e098a
doyeong <ddozero@naver.com>
2025-06-20 10:13:13 +0900

    ✨ Feat: add volunteer all list

--- care/src/main/java/com/animal/api/volunteers/service/UserVolunteersServcieImple.java
+++ care/src/main/java/com/animal/api/volunteers/service/UserVolunteersServcieImple.java
@@ -1,5 +1,32 @@
 package com.animal.api.volunteers.service;
 
-public interface UserVolunteersServcieImple {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.animal.api.volunteers.mapper.UserVolunteersMapper;
+import com.animal.api.volunteers.model.response.VolunteersListResponseDTO;
+
+@Service
+@Primary
+public class UserVolunteersServcieImple implements UserVolunteersService {
+
+	@Autowired
+	private UserVolunteersMapper mapper;
+
+	@Override
+	public List<VolunteersListResponseDTO> getAllVolunteers(int listSize, int cp) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+
+		map.put("listSize", listSize);
+		map.put("cp", cp);
+
+		List<VolunteersListResponseDTO> volunteerLists = mapper.getAllVolunteers(map);
+		return volunteerLists;
+	}
 
 }
