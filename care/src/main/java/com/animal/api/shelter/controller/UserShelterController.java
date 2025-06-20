830b878cc8ae2fe8f14223f5dcc3c95f508ebe71
Rege-97 <cogus1027@gmail.com>
2025-06-20 11:06:00 +0900

    ✨ Feat: update cotroller and query and test

--- care/src/main/java/com/animal/api/shelter/controller/UserShelterController.java
+++ care/src/main/java/com/animal/api/shelter/controller/UserShelterController.java
@@ -111,15 +111,34 @@ public class UserShelterController {
 
 	/**
 	 * 보호시설의 상세정보에서 해당 보호시설의 유기동물들을 조회
+	 * 
 	 * @param idx 보호시설의 idx
 	 * @param cp  봉사 컨텐츠의 현재 페이지
 	 * @return 해당 보호시설의 유기동물 리스트
 	 */
 	@GetMapping("/{idx}/animals")
 	public ResponseEntity<?> getAllShelterAnimals(@PathVariable int idx,
-			@RequestParam(value = "cp", defaultValue = "0") int cp) {
+			@RequestParam(value = "cp", defaultValue = "0") int cp,
+			@RequestParam(value = "type", required = false) String type,
+			@RequestParam(value = "breed", required = false) String breed,
+			@RequestParam(value = "gender", required = false) String gender,
+			@RequestParam(value = "neuter", defaultValue = "0") int neuter,
+			@RequestParam(value = "age", defaultValue = "0") int age,
+			@RequestParam(value = "adoptionStatus", required = false) String adoptionStatus,
+			@RequestParam(value = "personality", required = false) String personality,
+			@RequestParam(value = "size", defaultValue = "0") int size,
+			@RequestParam(value = "name", required = false) String name) {
 		int listSize = 3;
-		List<ShelterAnimalsDTO> animalList = service.getAllShelterAnimals(listSize, cp, idx);
+
+		List<ShelterAnimalsDTO> animalList = null;
+
+		if (type != null || breed != null || gender != null || neuter != 0 || age != 0 || adoptionStatus != null
+				|| personality != null || size != 0 || name != null) {
+			animalList = service.searchShelterAnimals(idx, listSize, cp, type, breed, gender, neuter, age,
+					adoptionStatus, personality, size, name);
+		} else {
+			animalList = service.getAllShelterAnimals(listSize, cp, idx);
+		}
 
 		if (animalList == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(400, "잘못된 접근입니다."));
