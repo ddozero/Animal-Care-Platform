5ed49c6287cf3bed4c9778cb2db2fc48166d7f89
Rege-97 <cogus1027@gmail.com>
2025-06-20 10:00:43 +0900

    ✨ Feat: add request dto and mapper query

---
+++ care/src/main/java/com/animal/api/shelter/model/request/SearchShelterAnimalRequestDTO.java
@@ -0,0 +1,21 @@
+package com.animal.api.shelter.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SearchShelterAnimalRequestDTO {
+	private int idx;
+	private int listSize;
+	private int cp;
+	private String type;
+	private String breed;
+	private String gender;
+	private int neuter;
+	private int age;
+	private String adoptionStatus;
+	private String personality;
+	private int size;
+	private String name;
+}
