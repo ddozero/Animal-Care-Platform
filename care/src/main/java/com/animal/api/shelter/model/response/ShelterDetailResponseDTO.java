f3555ef02159f70bcb09bec5100c0d4080bb22cb
Rege-97 <cogus1027@gmail.com>
2025-06-21 15:59:31 +0900

    ✏️ Chor: update dto and query

--- care/src/main/java/com/animal/api/shelter/model/response/ShelterDetailResponseDTO.java
+++ care/src/main/java/com/animal/api/shelter/model/response/ShelterDetailResponseDTO.java
@@ -2,9 +2,11 @@ package com.animal.api.shelter.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ShelterDetailResponseDTO {
 	private int idx;
 	private String shelterName;
--- care/src/main/java/com/animal/api/shelter/model/response/ShelterVolunteersResponseDTO.java
+++ care/src/main/java/com/animal/api/shelter/model/response/ShelterVolunteersResponseDTO.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ShelterVolunteersResponseDTO {
 	private int idx;
 	private String title;
