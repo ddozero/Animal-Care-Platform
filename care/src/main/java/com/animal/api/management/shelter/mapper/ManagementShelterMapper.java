55a3f233bf7996a3f0ddc94fe2a02f57689f9ec2
doyeong <kdoyeong1@gmail.com>
2025-06-21 13:41:07 +0900

    ✨ Feat: add shelter service

--- care/src/main/java/com/animal/api/management/shelter/mapper/ManagementShelterMapper.java
+++ care/src/main/java/com/animal/api/management/shelter/mapper/ManagementShelterMapper.java
@@ -1,5 +1,12 @@
 package com.animal.api.management.shelter.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.animal.api.management.shelter.model.response.AllManageShelterDTO;
+
+@Mapper
 public interface ManagementShelterMapper {
+
+	public AllManageShelterDTO getShelterInfo(int userIdx);
 
 }
--- care/src/main/java/com/animal/api/management/shelter/service/ShelterManageServiceImple.java
+++ care/src/main/java/com/animal/api/management/shelter/service/ShelterManageServiceImple.java
@@ -1,5 +1,26 @@
 package com.animal.api.management.shelter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.animal.api.management.shelter.mapper.ManagementShelterMapper;
+import com.animal.api.management.shelter.model.response.AllManageShelterDTO;
+
+@Service
+@Primary
 public class ShelterManageServiceImple implements ShelterManageService {
+
+	@Autowired
+	private ManagementShelterMapper mapper;
+
+	@Override
+	public AllManageShelterDTO getShelterInfo(int userIdx) {
+		AllManageShelterDTO dto = mapper.getShelterInfo(userIdx);
+		if (dto != null && dto.getDescription() != null) {
+			dto.setDescription(dto.getDescription().replaceAll("\n", "<br>"));
+		}
+		return dto;
+	}
 
 }
