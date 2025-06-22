6d1df0e32078de07acf9a203c399d4d6e75a3cd6
consgary <sesgodgood3@naver.com>
2025-06-22 16:11:27 +0900

    ✨ Feat: update requestDTO

--- care/src/main/java/com/animal/api/donation/model/request/DonationRequestDTO.java
+++ care/src/main/java/com/animal/api/donation/model/request/DonationRequestDTO.java
@@ -1,5 +1,7 @@
 package com.animal.api.donation.model.request;
 
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DonationRequestDTO {
+	@Positive(message = "회원번호는 0이상이어야 합니다.")
 	private int userIdx;
+	@Positive(message = "기부번호는 0이상이어야 합니다.")
 	private int donationIdx;
+	@Positive(message = "기부금액은 0이상이어야 합니다.")
 	private int donatedAmount;
 }
