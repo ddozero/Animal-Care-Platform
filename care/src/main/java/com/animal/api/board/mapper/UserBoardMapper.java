9197faf9486e565821491913894a9b29fb80937e
consgary <sesgodgood3@naver.com>
2025-06-23 12:48:11 +0900

    ✨ Feat: update dao

--- care/src/main/java/com/animal/api/board/mapper/UserBoardMapper.java
+++ care/src/main/java/com/animal/api/board/mapper/UserBoardMapper.java
@@ -1,5 +1,10 @@
 package com.animal.api.board.mapper;
 
-public interface UserBoardMapper {
+import java.util.List;
+import java.util.Map;
 
+import com.animal.api.board.model.response.AllBoardListResponseDTO;
+
+public interface UserBoardMapper {
+	public List<AllBoardListResponseDTO> getAllBoards(Map map);
 }
