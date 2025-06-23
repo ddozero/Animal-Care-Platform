46073b944959d14006aec56754e3968a08f186a1
Whistler95 <sdr020071@naver.com>
2025-06-23 12:10:47 +0900

    ✨ Feat modify password verify

--- care/src/main/java/com/animal/api/mypage/information/verify/service/PasswordVerifyService.java
+++ care/src/main/java/com/animal/api/mypage/information/verify/service/PasswordVerifyService.java
@@ -2,4 +2,6 @@ package com.animal.api.mypage.information.verify.service;
 
 public interface PasswordVerifyService {
 
+	//내 정보 수정 비밀번호 확인
+	boolean verifyPassword(int userIdx, String rawPassword);
 }
--- care/src/main/java/com/animal/api/mypage/information/verify/service/PasswordVerifyServiceImple.java
+++ care/src/main/java/com/animal/api/mypage/information/verify/service/PasswordVerifyServiceImple.java
@@ -1,5 +1,24 @@
 package com.animal.api.mypage.information.verify.service;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.animal.api.mypage.information.verify.mapper.PasswordVerifyMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Primary
+@RequiredArgsConstructor
 public class PasswordVerifyServiceImple implements PasswordVerifyService {
 
+	private final PasswordVerifyMapper passwordVerifyMapper;
+	private final BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	public boolean verifyPassword(int userIdx, String rawPassword) {
+		String encrytedPassword = passwordVerifyMapper.selectEncryptedPassword(userIdx);
+		return passwordEncoder.matches(rawPassword, encrytedPassword);
+	}
 }
