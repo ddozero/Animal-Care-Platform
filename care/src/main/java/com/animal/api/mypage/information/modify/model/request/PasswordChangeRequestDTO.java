95b09b5ed123c39aae7d2beccfb757907180386f
Whistler95 <sdr020071@naver.com>
2025-06-23 17:01:39 +0900

    ✨ Feat password change add captcha

--- care/src/main/java/com/animal/api/mypage/information/modify/model/request/PasswordChangeRequestDTO.java
+++ care/src/main/java/com/animal/api/mypage/information/modify/model/request/PasswordChangeRequestDTO.java
@@ -12,10 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PasswordChangeRequestDTO {
 
-    @NotBlank(message = "새 비밀번호는 필수 입력값입니다.")
-    @Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,20}$",
-        message = "비밀번호는 영문, 숫자, 특수문자 조합 8~20자여야 합니다."
-    )
-    private String newPassword;
+	@NotBlank(message = "현재 비밀번호는 필수 입력 항목입니다")
+	private String currentPassword;
+	
+	@NotBlank(message = "새 비밀번호는 필수 입력 항목입니다.")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{9,20}$", message = "비밀번호는 영문, 숫자, 특수문자 조합 9~20자여야 합니다.")
+	private String newPassword;
+	
+	@NotBlank(message = "새 비밀번호 확인은 필수 입력 항목입니다.")
+	private String confirmPassword;
+	
+	@NotBlank(message = "자동입력방지 인증 값은 필수 입력 항목입니다.")
+	private String captcha;
 }
