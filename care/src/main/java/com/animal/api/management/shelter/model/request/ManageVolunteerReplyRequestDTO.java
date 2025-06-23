2095bff48203d40f00f4937bb269b1c46ecad60b
doyeong <kdoyeong1@gmail.com>
2025-06-23 10:38:24 +0900

    🎨 Style: add vaild anotation shelter review reply request dto

--- care/src/main/java/com/animal/api/management/shelter/model/request/ManageVolunteerReplyRequestDTO.java
+++ care/src/main/java/com/animal/api/management/shelter/model/request/ManageVolunteerReplyRequestDTO.java
@@ -1,8 +1,7 @@
 package com.animal.api.management.shelter.model.request;
 
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,14 +12,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ManageVolunteerReplyRequestDTO {
 
-	private int reviewIdx;
+	@NotNull(message = "유저 IDX는 필수입니다.")
 	private int userIdx;
+
+	@NotNull(message = "봉사 목록 IDX는 필수입니다.")
 	private int volunteerIdx;
+
+	@NotBlank(message = "답글 내용 작성은 필수입니다.")
 	private String content;
+
+	@NotNull(message = "참조할 ref번호는 필수입니다.")
 	private int ref;
+
+	@NotNull(message = "lev번호는 필수입니다.")
 	private int lev;
+
+	@NotNull(message = "turn번호는 필수입니다.")
 	private int turn;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-	private Timestamp createdAt;
 
 }
