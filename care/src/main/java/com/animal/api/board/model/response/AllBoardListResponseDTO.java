72d2b036512b95c490f5f83fa4d87816906b4126
consgary <sesgodgood3@naver.com>
2025-06-23 12:44:19 +0900

    ✨ Feat: add sqlQuery and responseDTO

---
+++ care/src/main/java/com/animal/api/board/model/response/AllBoardListResponseDTO.java
@@ -0,0 +1,22 @@
+package com.animal.api.board.model.response;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllBoardListResponseDTO {
+	private int idx;
+	private String title;
+	private String nickname;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private Timestamp createdAt;
+	private int views;
+	private int likeCount;
+}
