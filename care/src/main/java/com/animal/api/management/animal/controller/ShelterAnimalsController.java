5de2fa0d2fc290a59f210fc32c38f0b5ee26ca47
Rege-97 <cogus1027@gmail.com>
2025-06-23 16:27:30 +0900

    ✨ Feat: update controller and test

--- care/src/main/java/com/animal/api/management/animal/controller/ShelterAnimalsController.java
+++ care/src/main/java/com/animal/api/management/animal/controller/ShelterAnimalsController.java
@@ -1,6 +1,7 @@
 package com.animal.api.management.animal.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +26,7 @@ import com.animal.api.common.model.ErrorResponseDTO;
 import com.animal.api.common.model.OkResponseDTO;
 import com.animal.api.management.animal.model.request.AnimalInsertRequestDTO;
 import com.animal.api.management.animal.model.request.AnimalUpdateRequestDTO;
+import com.animal.api.management.animal.model.response.AdoptionConsultListResponseDTO;
 import com.animal.api.management.animal.model.response.AnimalAddShelterInfoResponseDTO;
 import com.animal.api.management.animal.service.ShelterAnimalsService;
 
@@ -105,8 +108,9 @@ public class ShelterAnimalsController {
 
 	/**
 	 * 유기동물 이미지 업로드 메서드
+	 * 
 	 * @param files 프론트 영역에서 받은 유기동물 이미지
-	 * @param idx 유기동물이 생성되면서 생긴 관리번호
+	 * @param idx   유기동물이 생성되면서 생긴 관리번호
 	 * @return 파일 업로드 성공 또는 실패
 	 */
 	@PostMapping("/upload/{idx}")
@@ -196,6 +200,34 @@ public class ShelterAnimalsController {
 			return ResponseEntity.status(HttpStatus.OK).body(new OkResponseDTO<Void>(200, "유기동물 삭제 성공", null));
 		} else {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(400, "유기동물 삭제 실패"));
+		}
+	}
+
+	@GetMapping("/adoptions")
+	public ResponseEntity<?> getAdoptionConsultList(@RequestParam(value = "cp", defaultValue = "0") int cp,
+			HttpSession session) {
+		LoginResponseDTO loginUser = (LoginResponseDTO) session.getAttribute("loginUser");
+
+		if (loginUser == null) { // 로그인 여부 검증
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponseDTO(401, "로그인 후 이용해주세요."));
+		}
+
+		if (loginUser.getUserTypeIdx() != 2) { // 보호시설 회원 검증
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponseDTO(403, "보호시설 회원만 접근 가능합니다."));
+		}
+
+		int listSize = 3;
+
+		List<AdoptionConsultListResponseDTO> consultList = service.getAdoptionConsultList(loginUser.getIdx(), listSize,
+				cp);
+
+		if (consultList == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(400, "잘못된 요청 입니다."));
+		} else if (consultList.size() == 0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(404, "조회된 데이터가 없습니다."));
+		} else {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new OkResponseDTO<List<AdoptionConsultListResponseDTO>>(200, "조회 성공", consultList));
 		}
 	}
 
