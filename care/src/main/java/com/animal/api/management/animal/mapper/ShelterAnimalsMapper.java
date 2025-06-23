8d6a35b671cb02af19674acc5d0a7f5f6fbd2361
Rege-97 <cogus1027@gmail.com>
2025-06-23 16:12:30 +0900

    ✨ Feat: add response dto and mapper query

--- care/src/main/java/com/animal/api/management/animal/mapper/ShelterAnimalsMapper.java
+++ care/src/main/java/com/animal/api/management/animal/mapper/ShelterAnimalsMapper.java
@@ -1,9 +1,12 @@
 package com.animal.api.management.animal.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.animal.api.management.animal.model.request.AnimalInsertRequestDTO;
 import com.animal.api.management.animal.model.request.AnimalUpdateRequestDTO;
+import com.animal.api.management.animal.model.response.AdoptionConsultListResponseDTO;
 import com.animal.api.management.animal.model.response.AnimalAddShelterInfoResponseDTO;
 
 @Mapper
@@ -20,5 +23,7 @@ public interface ShelterAnimalsMapper {
 	public Integer getAnimalShelter(int idx);
 	
 	public Integer getAnimalMaxIdx();
+	
+	public AdoptionConsultListResponseDTO getAdoptionConsultList(Map map);
 
 }
