401be9c09feb0af06904b1456ed9f98396f737c2
Rege-97 <cogus1027@gmail.com>
2025-06-24 15:37:12 +0900

    ✨ Feat: add request dto and mapper query

--- care/src/main/java/com/animal/api/admin/board/mapper/AdminBoardMapper.java
+++ care/src/main/java/com/animal/api/admin/board/mapper/AdminBoardMapper.java
@@ -1,5 +1,12 @@
 package com.animal.api.admin.board.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AdminBoardMapper {
+
+	public Integer checkBoardType(int idx);
+
+	public int updateNotice(int idx);
 
 }
---
+++ care/src/main/java/com/animal/api/admin/board/model/request/NoticeUpdateRequestDTO.java
@@ -0,0 +1,29 @@
+package com.animal.api.admin.board.model.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoticeUpdateRequestDTO {
+
+	@NotNull(message = "게시글 번호는 필수입니다.")
+	private Integer idx;
+	
+	@NotNull(message = "게시글 제목은 필수입니다.")
+	@NotBlank(message = "게시글 제목을 적어주세요.")
+	@Size(max = 100, message = "제목을 100자 이내로 정해주세요.")
+	private String title;
+	
+	@NotNull(message = "게시글 본문은 필수입니다.")
+	@NotBlank(message = "게시글 본문을 적어주세요.")
+	@Size(max = 2000, message = "본문을 2000자 이내로 정해주세요.")
+	private String content;
+
+}
