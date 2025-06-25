35abd26ae42ad4a16c1659b52b4a33be1957d3d8
doyeong <kdoyeong1@gmail.com>
2025-06-25 17:07:38 +0900

    🐛 Fix: add file Upload

--- care/src/main/java/com/animal/api/management/shelter/model/request/ShelterBoardRequestDTO.java
+++ care/src/main/java/com/animal/api/management/shelter/model/request/ShelterBoardRequestDTO.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ShelterBoardRequestDTO {
 	
-	private Integer userIdx;
+	private int userIdx;
 	
 	@NotBlank(message = "제목 입력은 필수입니다.")
 	private String title;
