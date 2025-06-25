44df1bc91d063d8811e2a71ce9c352210b1a14a6
Rege-97 <cogus1027@gmail.com>
2025-06-25 21:12:05 +0900

    ✨ Feat: add service

--- care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterService.java
+++ care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterService.java
@@ -1,5 +1,12 @@
 package com.animal.api.admin.shelter.service;
 
 public interface AdminShelterService {
-
+	public static int POST_SUCCESS = 1;
+	public static int UPDATE_SUCCESS = 2;
+	public static int DELETE_SUCCESS = 3;
+	public static int UPLOAD_SUCCESS = 4;
+	public static int NOT_ANIMAL = 5;
+	public static int ERROR = -1;
+	
+	public int deleteAnimal(int animalIdx, int userIdx);
 }
--- care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterServiceImple.java
+++ care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterServiceImple.java
@@ -1,10 +1,33 @@
 package com.animal.api.admin.shelter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import com.animal.api.common.util.FileManager;
+import com.animal.api.management.animal.mapper.ShelterAnimalsMapper;
 
 @Service
 @Primary
 public class AdminShelterServiceImple implements AdminShelterService {
-	
+	@Autowired
+	private ShelterAnimalsMapper shelterAnimalsMapper;
+	@Autowired
+	private FileManager fileManager;
+
+	@Override
+	public int deleteAnimal(int animalIdx, int userIdx) {
+		Integer checkUserIdx = shelterAnimalsMapper.getAnimalShelter(animalIdx);
+		if (checkUserIdx == null) {// 해당 상담신청이 있는지 검증
+			return NOT_ANIMAL;
+		}
+
+		int result = shelterAnimalsMapper.deleteAnimal(animalIdx);
+		result = result > 0 ? DELETE_SUCCESS : ERROR;
+
+		if (result == DELETE_SUCCESS) {
+			fileManager.deleteFolder("animals", animalIdx);
+		}
+		return result;
+	}
 }
