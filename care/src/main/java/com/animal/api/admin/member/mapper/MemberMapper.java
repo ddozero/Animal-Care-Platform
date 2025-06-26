730d02ea34b3cc08029333e5e299df9b3c5be479
Whistler95 <sdr020071@naver.com>
2025-06-26 15:26:53 +0900

    ✨ Feat: admin member mapper

--- care/src/main/java/com/animal/api/admin/member/mapper/MemberMapper.java
+++ care/src/main/java/com/animal/api/admin/member/mapper/MemberMapper.java
@@ -1,5 +1,24 @@
 package com.animal.api.admin.member.mapper;
 
-public class MemberMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.animal.api.admin.member.model.response.MemberListResponseDTO;
+
+@Mapper
+public interface MemberMapper {
+
+	List<MemberListResponseDTO> selectMember(@Param("keyword") String keyword,
+											 @Param("type") String type,
+											 @Param("userType") Integer userType,
+											 @Param("status") Integer status,
+											 @Param("offset") int offset,
+											 @Param("size") int size);
+	
+	int countMember(@Param("keyword") String keyword,
+					@Param("type") String type,
+					@Param("userType") Integer userType,
+					@Param("status") Integer status);
 }
