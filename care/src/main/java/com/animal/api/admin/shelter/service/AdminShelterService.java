aee6e8b07c013dda37b4de3b5f4eaa10d5330368
Rege-97 <cogus1027@gmail.com>
2025-06-26 14:28:30 +0900

    ✨ Feat: add service

--- care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterService.java
+++ care/src/main/java/com/animal/api/admin/shelter/service/AdminShelterService.java
@@ -6,7 +6,15 @@ import com.animal.api.admin.shelter.model.response.ShelterJoinRequestListRespons
 
 public interface AdminShelterService {
 
+	static int UPDATE_SUCCESS = 1;
+	static int NOT_REQUEST = 2;
+	static int APPROVED = 3;
+	static int WITHDRAWN = 4;
+	static int ERROR = -1;
+
 	public List<ShelterJoinRequestListResponseDTO> getShelterJoinRequestList(int listSize, int cp);
-	
+
 	public ShelterJoinRequestListResponseDTO getShelterJoinRequestDetail(int idx);
+
+	public int updateShelterJoinRequestStatus(int idx);
 }
